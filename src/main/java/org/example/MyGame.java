7ca417b50d72fa1a00796fb45a793bfc52f123b2
Umang Gupta <110466519+Umangg-singhal@users.noreply.github.com>
2022-10-09 16:55:11 +0530

    My Gaming Project Uploading
    Files uploading

---
+++ src/main/java/org/example/MyGame.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class MyGame {
+
+    public static void loginPage(){
+        System.out.println("Inside Login Page");
+    }
+
+    public static void userName(){
+
+    }
+    public static void main(String[] args) {
+        loginPage();
+    }
+}
